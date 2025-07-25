edfe707a2a6175a162c73861adad97e8e6b4af13
Paul Dejean <paulcdejean@gmail.com>
2025-07-25 18:32:24 -0500

    Added some comments.

--- listener/src/main.rs
+++ listener/src/main.rs
@@ -1,19 +1,23 @@
 mod c_uuid {
     use std::mem::MaybeUninit;
-    #[allow(non_camel_case_types)] // I want to use the same type name that the C header is using
+    // I want to use the same type name that the C header is using
+    #[allow(non_camel_case_types)]
     pub type uuid_t = [std::ffi::c_uchar; 16];
     unsafe extern "C" {
         pub unsafe fn uuid_generate(out: *mut MaybeUninit<uuid_t>);
     }
 }
+
 use std::mem::MaybeUninit;
 
 fn main() {
     let new_uuid: u128 = uuid_generate();
-    println!("{:?}", new_uuid)
+    println!("{new_uuid}")
 }
 
 pub fn c_uuid_to_u128(uuid: c_uuid::uuid_t) -> u128 {
+    // There's several open clippy issues about this
+    #[allow(clippy::unnecessary_cast)]
     u128::from_ne_bytes(uuid.map(|b| b as u8))
 }
 

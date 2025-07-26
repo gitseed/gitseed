65ae4ae44cd709dd618330d1f9edbc3f09b261bc
Paul Dejean <paulcdejean@gmail.com>
2025-07-25 20:29:38 -0500

    Working!!!

--- listener/src/pq.rs
+++ listener/src/pq.rs
@@ -1,14 +1,12 @@
 use crate::libpq;
 
 pub fn connect(conninfo: &str) -> *const libpq::PGconn {
-    let raw_conninfo: *mut std::ffi::c_char = std::ffi::CString::new(conninfo).expect("Postgres connection info should not contain internal null characters") .into_raw(); 
-    unsafe {
-        libpq::PQconnectdb(raw_conninfo)
-    }
+    let raw_conninfo: *mut std::ffi::c_char = std::ffi::CString::new(conninfo)
+        .expect("Postgres connection info should not contain internal null characters")
+        .into_raw();
+    unsafe { libpq::PQconnectdb(raw_conninfo) }
 }
 
 pub fn server_version(conn: *const libpq::PGconn) -> i64 {
-    (unsafe {
-        libpq::PQserverVersion(conn)
-    }) as i64
+    (unsafe { libpq::PQserverVersion(conn) }) as i64
 }
